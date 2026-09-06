daa76e1ef798098b0e861bb25e0c9d53a0550b6d
Evan Martin <evan.martin@gmail.com>
2026-09-05 18:15:34 -0700

    rearrange

--- src/analyze.rs
+++ src/analyze.rs
@@ -19,44 +19,23 @@ pub fn run(db: &mut DB, _args: Args) -> anyhow::Result<()> {
         .filter_map(|func| Some((func.ip, func.name.as_ref()?.clone())))
         .collect::<HashMap<_, _>>();
     for func in db.functions.values_mut() {
-        analyze(&names, func);
+        analyze(func, |ip| match names.get(&ip) {
+            Some(name) => XRef::Name(name.clone()),
+            None => XRef::Addr(ip),
+        });
     }
 
     db.write()?;
     Ok(())
 }
 
-fn analyze(names: &HashMap<SegOfs, String>, func: &mut Function) {
+fn analyze(func: &mut Function, xref: impl Fn(SegOfs) -> XRef) {
     let mut all_xrefs = HashSet::new();
     for block in func.blocks.iter_mut() {
         let ip = block.ip;
         for instr in block.instrs.iter_mut() {
-            use iced_x86::FlowControl::*;
-            let xref = match instr.iced.flow_control() {
-                Next | Return | Interrupt => continue,
-                Call | IndirectCall | IndirectBranch | UnconditionalBranch | ConditionalBranch => {
-                    use iced_x86::OpKind::*;
-                    match instr.iced.op0_kind() {
-                        NearBranch16 | FarBranch16 => {
-                            let ip = match instr.iced.op0_kind() {
-                                NearBranch16 => ip.with_ofs(instr.iced.near_branch16()),
-                                FarBranch16 => {
-                                    (instr.iced.far_branch_selector(), instr.iced.far_branch16())
-                                        .into()
-                                }
-                                _ => unreachable!(),
-                            };
-                            match names.get(&ip) {
-                                Some(name) => XRef::Name(name.clone()),
-                                None => XRef::Addr(ip),
-                            }
-                        }
-                        Memory => XRef::Name("mem?".into()),
-                        Register => XRef::Name("reg?".into()),
-                        d => todo!("unhandled jmp {d:?}"),
-                    }
-                }
-                XbeginXabortXend | Exception => todo!(),
+            let Some(xref) = xref_from_instr(ip, &instr.iced, &xref) else {
+                continue;
             };
             instr.jmp = Some(xref.clone());
             all_xrefs.insert(xref);
@@ -66,5 +45,33 @@ fn analyze(names: &HashMap<SegOfs, String>, func: &mut Function) {
     if !xrefs.is_empty() {
         xrefs.sort();
         func.xrefs = Some(xrefs);
+    }
+}
+
+fn xref_from_instr(
+    ip: SegOfs,
+    instr: &iced_x86::Instruction,
+    xref: impl Fn(SegOfs) -> XRef,
+) -> Option<XRef> {
+    use iced_x86::FlowControl::*;
+    match instr.flow_control() {
+        Next | Return | Interrupt => None,
+        Call | IndirectCall | IndirectBranch | UnconditionalBranch | ConditionalBranch => {
+            use iced_x86::OpKind::*;
+            match instr.op0_kind() {
+                NearBranch16 | FarBranch16 => {
+                    let ip = match instr.op0_kind() {
+                        NearBranch16 => ip.with_ofs(instr.near_branch16()),
+                        FarBranch16 => (instr.far_branch_selector(), instr.far_branch16()).into(),
+                        _ => unreachable!(),
+                    };
+                    Some(xref(ip))
+                }
+                Memory => Some(XRef::Name("mem?".into())),
+                Register => Some(XRef::Name("reg?".into())),
+                d => todo!("unhandled jmp {d:?}"),
+            }
+        }
+        XbeginXabortXend | Exception => todo!(),
     }
 }

28fd0b7630b15ecedfa8169ac58efed4aea09b1f
Evan Martin <evan.martin@gmail.com>
2026-09-07 20:43:50 -0700

    render jump comment

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -140,9 +140,13 @@ function FunctionView({ ip }: { ip: string }) {
             {func.blocks.length === 0 ? <p>No instructions found.</p> : func.blocks.map((block, index) => (
               <section key={index}>
                 {block.instrs.length === 0 ? <p>No instructions in this block.</p> : (
-                  <pre><code>{block.instrs.map(instr =>
-                    `${instr.comment ? instr.comment.split('\n').map(line => `; ${line}\n`).join('') : ''}${instr.ip} ${instr.text}`
-                  ).join('\n')}</code></pre>
+                  <pre><code>{block.instrs.map(instr => {
+                    const comment = instr.comment
+                      ? instr.comment.split('\n').map(line => `; ${line}\n`).join('')
+                      : '';
+                    const jumpComment = instr.jmp !== null ? ` ; ${instr.jmp}` : '';
+                    return `${comment}${instr.ip} ${instr.text}${jumpComment}`;
+                  }).join('\n')}</code></pre>
                 )}
               </section>
             ))}
